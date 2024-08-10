1a1e3b56f2c8f725d5742d979b9668627ed9f536
ekj <kuochiayung@gmail.com>
2024-08-11 02:51:45 +0800

    add rgb image part

--- inc/rgb_image.h
+++ inc/rgb_image.h
@@ -5,9 +5,15 @@
 
 class RGBImage : public Image{
 private:
-
+    int ***pixels;
 public:
-
+    RBGImage();
+    RBGImage(int width, int height, int ***pixels);
+    ~RGBImage();
+    bool LoadImage(string file);
+    void DumpImage(string file);
+    void Display_ASCII();
+    void Display_CMD();
 };
 
 #endif
---
+++ src/rgb_image.cpp
@@ -0,0 +1,39 @@
+#include "rgb_image.h"
+
+RGBImage::RGBImage() : Image(0, 0), pixels(nullptr){};
+
+RGBImage::RGBImage(int width, int height, int ***pixels) : Image(width, height), pixels(pixels){};
+
+RGBImage::~RGBImage() {
+    for(int i = 0; i < h; i++) {
+        for(int q = 0; q < w; q++) {
+            delete[] pixels[i][q];
+        }delete[] pixels[i];
+    }
+    delete[] pixels;
+    return;
+}
+
+bool RGBImage::LoadImage(string file) {
+    pixels = data_loader.Load_RGB(file, &w, &h); // get pixels, w, h based on file
+    return(pixels != nullptr);
+}
+
+void RGBImage::DumpImage(string file) {
+    data_loader.Dump_RGB(w, h, pixels, file);
+    return;
+}
+
+void RGBImage::Display_ASCII() {
+    data_loader.Display_RGB_ASCII(w, h, pixels);
+    return;
+}
+
+void RGBImage::Display_CMD() {
+    string file = "CMD_RGB.jpg";
+    data_loader.DumpImage(w, h, pixels, file);
+    data_loader.Display_RGB_ASCII(file);
+    const string cmd_str = "rm" + file;
+    system(cmd_str.c_str());
+    return;
+}
